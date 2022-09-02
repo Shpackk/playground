626ab1c7942e903ebfad3810249e7793e3d779f4
Shpackk <victorija45@gmail.com>
2022-09-02 19:11:32 +0300

    Feature: reflect class test: PG-2

---
+++ features/reflect.ts
@@ -0,0 +1,15 @@
+class User {
+    public name: string;
+    public age: number;
+    public height: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+        this.height = 170
+    }
+}
+
+// create instance
+const UserInstance = Reflect.construct(User, ['Adam', 10])
+// true for 'name' property
+const hasName = Reflect.has(UserInstance, 'name')
